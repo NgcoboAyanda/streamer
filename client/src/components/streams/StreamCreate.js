08f8b20e965b538f1941f787a71f4d1b617d1cb8
Ayandaa <ayanda13@protonmail.com>
2021-01-31 12:23:15 +0200

    fixed create stream not working

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,14 +3,18 @@ import { connect } from 'react-redux';
 
 import StreamForm from './StreamForm';
 import { createStream } from '../../actions';
+import { formValues } from 'redux-form';
 
 class StreamCreate extends React.Component{
+    createStream=(formValues)=>{
+        this.props.createStream(formValues);
+    }
 
     render(){
         return(
             <div>
                 <StreamForm
-                    onSubmit = {this.props.createStream}
+                    onFormSubmit = {this.createStream}
                 />
             </div>
         )

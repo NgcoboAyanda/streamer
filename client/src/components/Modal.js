10614c21f15b5c519dff484163dc53c54ad583ea
Ayandaa <ayanda13@protonmail.com>
2021-02-06 11:08:15 +0200

    modal now takes props instead of one config object

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-/* the Modal component is passed a config object that looks like this
+/* the Modal component is passed props to configure it*/
 
- {
-    TITLE:  The title here,
-    CONFIRM-MSG:  The msg that asks if you want to continue,
-    ACTION:  Jsx that renders action buttons,
-    ON-DISMISS: Function to do something when user dismisses modal
- }
-*/
 
-const Modal = ({config}) => {//config is destructured from props object
+
+const Modal = (props) => {//config is destructured from props object
     return ReactDOM.createPortal(
-        <div onClick={config.onDismiss} className="ui dimmer modals visible active">
+        <div onClick={props.onDismiss} className="ui dimmer modals visible active">
             <div onClick={e=>e.stopPropagation()} className="ui standard modal visible active">
                 <div className="header">
-                    {config.title}
+                    {props.title}
                 </div>
                 <div className="content">
-                    {config.confirmMsg}
+                    {props.confirmMsg}
                 </div>
                 <div className="actions">
-                    {config.actions}
+                    {props.actions}
                 </div>
             </div>
         </div>,

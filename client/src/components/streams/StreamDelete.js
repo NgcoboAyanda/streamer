2fc90b329e5285247e7ee22daf09efc443799e20
Ayandaa <ayanda13@protonmail.com>
2021-02-06 11:07:27 +0200

    fixed streamDel not showing title on page refresh

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -6,36 +6,55 @@ import Modal from '../Modal';
 import history from '../../history';
 
 class StreamDelete extends React.Component{
+    id = this.props.match.params.streamId;
     componentDidMount(){
-        this.props.fetchStream(this.props.match.params.streamId);
+        this.props.fetchStream(this.id);
+    }
+
+    removeStream = ()=>{
+        this.props.deleteStream(this.id);
+    }
+
+    dismissModal = ()=>{
+        history.push('/');
     }
 
     //<> is short for React.Fragment
     modalActions = (
         <> 
-            <button className="ui button negative">
+            <button className="ui button negative" onClick={this.removeStream}>
                 Delete
             </button>
-            <button className="ui button">
+            <button className="ui button" onClick={this.dismissModal}>
                 Cancel
             </button>
         </>
     )
 
-    modalConfig = {
-        title: "Delete Stream",
-        confirmMsg: `Are you sure you want to delete ${this.props.stream.title} stream?`,
-
-        actions: this.modalActions,
-        onDismiss: e=>history.push('/')
+    renderStreamTitle = (props)=>{
+        const {stream} = props;
+        let title;
+        if(stream){
+            title = ` "${stream.title}" `
+        }
+        else{
+            title = ''
+        }
+        return title;
     }
 
     render() {
-        return(
-            <div>
-                <Modal config={this.modalConfig}/>
-            </div>
-        )
+            return(
+                <div>
+                    <Modal 
+                        title={'Delete Stream'}
+                        confirmMsg = {`Are you sure you want to delete ${this.renderStreamTitle(this.props)} stream?`}
+                        actions = {this.modalActions}
+                        onDismiss = {this.dismissModal}
+                    />
+                </div>
+            )
+
     }
 }
 
